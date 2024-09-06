746f6968d3e08403cb367766081f35738b593779
BrightSky-OSSDO <158616860+BrightSky-OSSDO@users.noreply.github.com>
2024-09-06 02:41:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function search() {
+    const searchQuery = document.getElementById("searchBox").value;
+    const encodedText = encodeURIComponent(searchQuery);
+    window.location.href = window.location.pathname + "?search=" + encodedText;
+}
+
+function search2() {
+    const searchQuery2 = document.getElementById("search2").value;
+    const encodedText2 = encodeURIComponent(searchQuery2);
+    window.location.href = window.location.pathname + "?search=" + encodedText2;
+}
+
+function retrieveQuery() {
+    const queryString = window.location.search;
+    const urlParameters = new URLSearchParams(queryString);
+    const decodedQuery = decodeURIComponent(queryString);
+    console.log(decodedQuery.replace("?search=", ""));
+
+    if (decodedQuery == "") {
+        document.getElementById("view1").style.display = "block";
+        document.getElementById("view2").style.display = "none";
+    } else {
+        document.getElementById("view1").style.display = "none";
+        document.getElementById("view2").style.display = "block";
+        document.getElementById("searchTerm").textContent = decodedQuery.replace("?search=", "");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    retrieveQuery();
+});
